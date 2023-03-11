105b70b7b8e9deae688286cb13697688069d4722
Hoang Cuong Nguyen <hcuong.ng.ca@gmail.com>
2023-03-11 12:37:10 -0800

    choose your learning page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="mockup-phone">
+      <div className="camera"></div>
+      <div className="display">
+        <div className="artboard artboard-demo phone-5 center">
+          <App />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>,
 )
