fa4d9ad4ba9888c5647f2b1337143b95a4c513f0
hlee443 <hlee443@my.bcit.ca>
2023-03-11 22:22:17 -0800

    api modal

--- src/components/ModalButton.jsx
+++ src/components/ModalButton.jsx
@@ -1,67 +1,102 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const ModalButton = ({ correct }) => {
+const goodTitles = [
+  "Good answer!",
+  "Great job!",
+  "You got it!",
+  "Correct!",
+  "Nice!",
+  "Awesome!",
+  "Well done!",
+  "POGGERS?!?!",
+];
+
+const badTitles = [
+  "Not quite...",
+  "Incorrect!",
+  "Wrong!",
+  "Nope!",
+  "Not quite!",
+  "Nice try!",
+  "NOT POGGERS >:(",
+];
+
+const ModalButton = ({ correct, onClick }) => {
   const navigate = useNavigate();
 
-  if (correct) {
-    const title = "Correct";
-    const answer = "Why the answer is incorrect/correct";
+  
+
+  if (correct.response_quality === true) {
+    const title = goodTitles[Math.floor(Math.random() * goodTitles.length)];
+    const answer = correct.response_content;
 
     return (
       <div className="">
         {/* The button to open modal */}
-        <label htmlFor="my-modal-6" className="btn btn-wide text-lg">
+        <label
+          htmlFor="my-modal-6"
+          className="btn btn-wide text-lg"
+          onClick={onClick}
+        >
           Submit
         </label>
 
         {/* Put this part before </body> tag */}
         <input type="checkbox" id="my-modal-6" className="modal-toggle" />
-        <div className="modal modal-bottom sm:modal-middle">
-          <div className="modal-box rounded-none border-t-4 border-primary bg-secondary">
-            <h3 className="font-bold text-4xl text-primary">{title}</h3>
-            <p className="py-4 text-lg text-primary">{answer}</p>
-            <div className="modal-action justify-center">
-              <label
-                htmlFor="my-modal-6"
-                className="btn btn-wide text-xl btn-primary text-white"
-                onClick={() => navigate("/gameover")}
-              >
-                Continue
-              </label>
+        {!answer || (
+          <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box rounded-none border-t-4 border-primary bg-secondary">
+              <h3 className="font-bold text-4xl text-primary">{title}</h3>
+              <p className="py-4 text-lg text-primary">{answer}</p>
+              <div className="modal-action justify-center">
+                <label
+                  htmlFor="my-modal-6"
+                  className="btn btn-wide text-xl btn-primary text-white"
+                  onClick={() => navigate("/gameover")}
+                >
+                  Continue
+                </label>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   } else {
-    const title = "Incorrect";
-    const answer = "Why the answer is incorrect/correct";
+    const title = badTitles[Math.floor(Math.random() * badTitles.length)]
+    const answer = correct.response_content;
 
     return (
       <div className="">
         {/* The button to open modal */}
-        <label htmlFor="my-modal-6" className="btn btn-wide text-lg">
+        <label
+          htmlFor="my-modal-6"
+          className="btn btn-wide text-2xl w-80"
+          onClick={onClick}
+        >
           Submit
         </label>
 
         {/* Put this part before </body> tag */}
         <input type="checkbox" id="my-modal-6" className="modal-toggle" />
-        <div className="modal modal-bottom sm:modal-middle">
-          <div className="modal-box rounded-none border-t-4 border-error bg-accent">
-            <h3 className="font-bold text-4xl text-red-600">{title}</h3>
-            <p className="py-4 text-lg text-red-600">{answer}</p>
-            <div className="modal-action justify-center">
-              <label
-                htmlFor="my-modal-6"
-                className="btn btn-wide text-xl bg-accent-focus border-rose-400 hover:bg-error"
-                onClick={() => navigate("/gameover")}
-              >
-                Continue
-              </label>
+        {!answer || (
+          <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box rounded-none border-t-4 border-error bg-accent">
+              <h3 className="font-bold text-4xl text-red-600">{title}</h3>
+              <p className="py-4 text-lg text-red-600">{answer}</p>
+              <div className="modal-action justify-center">
+                <label
+                  htmlFor="my-modal-6"
+                  className="btn btn-wide text-xl bg-accent-focus border-rose-400 hover:bg-error"
+                  onClick={() => navigate("/gameover")}
+                >
+                  Continue
+                </label>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
